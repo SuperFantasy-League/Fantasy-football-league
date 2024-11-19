ee7ca820b4b3d99b194134c994d076100f66d094
Michael Nwachukwu <dahformulah@gmail.com>
2024-11-19 10:00:45 +0100

    updates

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-20 py-7 min-h-screen h-screen`}
-      >
-        {children}
-      </body>
+      <ThirdwebProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased px-20 py-7 min-h-screen h-screen`}
+        >
+          {children}
+        </body>
+      </ThirdwebProvider>
     </html>
   );
 }
