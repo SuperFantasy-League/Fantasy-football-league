3cb046bee8c7aa87e619dc11bc899fafce4b294b
frankie-powers <franklin.power21@gmaul.com>
2024-11-23 14:39:05 +0100

    new changes

--- client/app/dashboard/betting/page.tsx
+++ client/app/dashboard/betting/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div>loading...</div>;
+  return (
+    <div className=" flex justify-center place-items-center">
+      <Image
+        src={"/ComingSoon.png"}
+        alt={"coming soon"}
+        layout="fill"
+        objectFit="cover"
+        className="rounded-t-lg"
+      />
+    </div>
+  );
 };
 
 export default page;
--- client/app/dashboard/wager/page.tsx
+++ client/app/dashboard/wager/page.tsx
@@ -1,34 +1,18 @@
-"use client";
-import { useState, useEffect } from "react";
-import TeamDisplay from "@/components/macro/TeamDisplay";
+import Image from "next/image";
+import React from "react";
 
-export default function TeamsPage() {
-  const [teams, setTeams] = useState([]);
-  const [loading, setLoading] = useState(true);
+const page = () => {
+  return (
+    <div className=" flex justify-center place-items-center">
+      <Image
+        src={"/ComingSoon.png"}
+        alt={"coming soon"}
+        layout="fill"
+        objectFit="cover"
+        className="rounded-t-lg"
+      />
+    </div>
+  );
+};
 
-  useEffect(() => {
-    const fetchTeams = async () => {
-      try {
-        const response = await fetch(
-          "https://node-backend-7sxv.onrender.com/api/teams/get-teams?page=2"
-        );
-        const data = await response.json();
-        console.log(data?.data?.response?.data);
-        if (!data?.data?.response?.data) return;
-        setTeams(data?.data?.response?.data);
-      } catch (error) {
-        console.error("Error fetching teams:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchTeams();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return <TeamDisplay teams={teams} />;
-}
+export default page;
