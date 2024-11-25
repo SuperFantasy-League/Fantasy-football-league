ce3c63a8e4731e55a7bb0c02a61662dfe25d3ee5
Michael Nwachukwu <dahformulah@gmail.com>
2024-11-25 15:15:45 +0100

    user points tatistics bar chart

---
+++ client/components/macro/PointsChart.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+    { month: "Week-1", desktop: 186 },
+    { month: "Week-2", desktop: 305 },
+    { month: "Week-3", desktop: 237 },
+    { month: "Week-4", desktop: 73 },
+    { month: "Week-5", desktop: 209 },
+    { month: "Week-6", desktop: 214 },
+]
+
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "hsl(var(--chart-1))",
+    },
+} satisfies ChartConfig
+
+export function PointsChart() {
+    return (
+        <Card className="shadow-md">
+            <CardHeader>
+                <CardTitle>Points through the season</CardTitle>
+                <CardDescription>January - June 2024</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
+                            <LabelList
+                                position="top"
+                                offset={12}
+                                className="fill-foreground"
+                                fontSize={12}
+                            />
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col items-start gap-2 text-sm">
+                <div className="flex gap-2 font-medium leading-none">
+                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
