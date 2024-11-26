99673d1f95736c4c12f304c79deeec1fb93c3289
frankie-powers <franklin.power21@gmaul.com>
2024-11-26 10:40:10 +0100

    typo error

--- client/app/dashboard/betting/page.tsx
+++ client/app/dashboard/betting/page.tsx
@@ -1,28 +1,26 @@
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const page = () => {
-    return (
-        <section className="flex justify-center items-center w-full h-full">
-            <div className="flex flex-col items-center gap-4">
-                <Image
-                    src={'/bolt.svg'}
-                    alt="bolt icon for coming soon"
-                    width={250}
-                    height={250}
-                />
-                <p className="text-3xl fo tracking-tighter">
-                    Betting is Unavailable at this time, <br /> check out the faantasy Page
-                </p>
-                <Link href={'/fantasy'}>
-                    <Button className="w-32 h-10">
-                        Fantasy
-                    </Button>
-                </Link>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="flex justify-center items-center w-full h-full">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src={"/bolt.svg"}
+          alt="bolt icon for coming soon"
+          width={250}
+          height={250}
+        />
+        <p className="text-3xl fo tracking-tighter">
+          Betting is Unavailable at this time, <br /> check out the fantasy Page
+        </p>
+        <Link href={"/fantasy"}>
+          <Button className="w-32 h-10">Fantasy</Button>
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
