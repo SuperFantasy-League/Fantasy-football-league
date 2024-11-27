e4c0d0cf47757015ed3cf4a82b9ceb0241f7e8e5
frankie-powers <franklin.power21@gmaul.com>
2024-11-27 13:18:21 +0100

    latest changes

---
+++ client/app/dashboard/fantasy/[league]/usermatches/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Fixtures from "@/components/macro/Fixtures";
+
+const page = () => {
+  return (
+    <div className="w-3/4">
+      <Fixtures />
+    </div>
+  );
+};
+
+export default page;
